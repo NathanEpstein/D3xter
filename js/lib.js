ae5b645341e622392b45344b482874922619052f
NathanEpstein <ne2210@columbia.edu>
2016-03-07 18:03:47 -0500

    replace start function with modular private methods

--- js/lib.js
+++ js/lib.js
@@ -1,47 +1,106 @@
-// START FUNCTION: This function creates the canvas, the everything bundle within the canvas, the axes (given scale functions), and the axes labels
-start = function(xLab,yLab,xMap,yMap,canvasWidth,canvasHeight,width,height,selector){
+function D3xter(config) {
+  var self = this;
 
-  var canvas = d3.select(selector)
-                .append('svg')
-                .attr('height',canvasHeight)
-                .attr('width', canvasWidth);
+  var height = config.height || 500,
+      width = config.width || 500;
 
-  var everything = canvas.append('g');
+  var margin = {
+    top: height * 0.05,
+    bottom: height * 0.2,
+    left: width * 0.2,
+    right: width * 0.05
+  };
 
-  everything.attr('transform','translate('+(width * 0.2)+','+height*0.1+')');
+  // private methods
+  function buildCanvas() {
+    self.canvas = d3.select(config.selector || 'body')
+                  .append('svg')
+                  .attr('height', height)
+                  .attr('width', width);
+  };
 
-  var xAxis = d3.svg.axis()
-              .scale(xMap);
+  function buildXMap() {
+    var values = datasets.map(function(d) { return d.x }).reduce(function(a, b) { return a.concat(b) });
+    var ordinalX = (typeof values[0] == 'string');
 
-  var yAxis = d3.svg.axis()
-              .scale(yMap)
-              .orient('left');
+    if (ordinalX) {
+      var xDomain = getUniqueValues(values);
+      self.xMap = d3.scale.ordinal()
+    }
+    else {
+      var xDomain = getBoundaries(values);
+      self.xMap = d3.scale.linear()
+    };
 
-  everything.append('g')
-        .attr('transform','translate(0,'+height+')')
-        .call(xAxis);
+    self.xMap.domain(xDomain)
+        .range([
+          margin.left,
+          width - margin.right
+        ]);
 
-  everything.append('g')
-        .call(yAxis);
+  };
 
-  var xLabel = everything.append('text')
-              .attr('x',canvasWidth*0.4)
-              .attr('y',height+45)
-              .text(xLab)
-              .attr('text-anchor','middle');
+  function buildYMap() {
+    var values = datasets.map(function(d) { return d.y }).reduce(function(a, b) { return a.concat(b) });
 
-  var yLabel = everything.append('text')
-              .attr('x', -canvasHeight*0.4)
-              .attr('y', -canvasWidth*0.1)
-              .attr('transform','rotate(-90)')
-              .text(yLab)
-              .attr('text-anchor','middle');
+    var yDomain = getBoundaries(values);
+    self.yMap = d3.scale.linear()
+               .domain(yDomain)
+               .range([
+                  height - margin.bottom,
+                  margin.top
+                ]);
+  };
 
-  var objects = [canvas,everything];
-  return objects;
+  function getBoundaries(data) {
+    var min = Math.min.apply(null, data);
+    var max = Math.max.apply(null, data);
+    return [min, max];
+  };
 
-}
-// END OF START FUNCTION
+  function getUniqueValues(data) {
+    var seenValues = {}, uniques = [];
+    data.forEach(function(val) {
+      if (seenValues[val] != 1) {
+        seenValues[val] = 1;
+        uniques.push(val);
+      };
+    });
+    return uniques;
+  };
+
+  function buildAxes() {
+    var xAxis = d3.svg.axis()
+                .scale(self.xMap);
+
+    var yAxis = d3.svg.axis()
+                .scale(self.yMap)
+                .orient('left');
+
+    self.cavas.append('g')
+          .attr('transform','translate(0,' + height - margin.bottom + ')')
+          .call(xAxis);
+
+    self.cavas.append('g')
+          .attr('transform','translate(' + margin.left + ', 0)')
+          .call(yAxis);
+  };
+
+  function buildAxisLabels() {
+    var xLabel = self.buffer.append('text')
+                .attr('x', width / 2)
+                .attr('y', height + margin.bottom / 2)
+                .text(config.xLab)
+                .attr('text-anchor','middle');
+
+    var yLabel = self.buffer.append('text')
+                .attr('x', height / 2)
+                .attr('y', margin.left / 2)
+                .attr('transform','rotate(90)')
+                .text(config.yLab)
+                .attr('text-anchor','middle');
+  };
+};
 
 pie = function(data,config){
   config = config || {};
