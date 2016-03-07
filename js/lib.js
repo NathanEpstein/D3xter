1ce23ff8971b04df79a9ae575bd41c910ed3dee2
NathanEpstein <ne2210@columbia.edu>
2016-03-07 18:28:53 -0500

    reorder functions and add build method

--- js/lib.js
+++ js/lib.js
@@ -11,7 +11,6 @@ function D3xter(config) {
     right: width * 0.05
   };
 
-  // private methods
   function buildCanvas() {
     self.canvas = d3.select(config.selector || 'body')
                   .append('svg')
@@ -19,11 +18,16 @@ function D3xter(config) {
                   .attr('width', width);
   };
 
-  function buildXMap() {
-    var values = datasets.map(function(d) { return d.x }).reduce(function(a, b) { return a.concat(b) });
-    var ordinalX = (typeof values[0] == 'string');
+  function buildMapping(datasets) {
+    buildXMap(datasets);
+    buildYMap(datasets);
+  };
 
-    if (ordinalX) {
+  function buildXMap(datasets) {
+    var values = datasets.map(function(d) { return d.x }).reduce(function(a, b) { return a.concat(b) });
+    var ordinalValues = (typeof values[0] == 'string');
+
+    if (ordinalValues) {
       var xDomain = getUniqueValues(values);
       self.xMap = d3.scale.ordinal()
     }
@@ -40,7 +44,7 @@ function D3xter(config) {
 
   };
 
-  function buildYMap() {
+  function buildYMap(datasets) {
     var values = datasets.map(function(d) { return d.y }).reduce(function(a, b) { return a.concat(b) });
 
     var yDomain = getBoundaries(values);
@@ -50,23 +54,6 @@ function D3xter(config) {
                   height - margin.bottom,
                   margin.top
                 ]);
-  };
-
-  function getBoundaries(data) {
-    var min = Math.min.apply(null, data);
-    var max = Math.max.apply(null, data);
-    return [min, max];
-  };
-
-  function getUniqueValues(data) {
-    var seenValues = {}, uniques = [];
-    data.forEach(function(val) {
-      if (seenValues[val] != 1) {
-        seenValues[val] = 1;
-        uniques.push(val);
-      };
-    });
-    return uniques;
   };
 
   function buildAxes() {
@@ -100,359 +87,29 @@ function D3xter(config) {
                 .text(config.yLab)
                 .attr('text-anchor','middle');
   };
-};
 
-pie = function(data,config){
-  config = config || {};
-  config.labels = config.labels || data;
-  var selector = config.selector || 'body',
-      height = config.height || 500,
-      width = config.width || 500;
-      canvas = d3.select(selector).append('svg')
-          .attr('height',height)
-          .attr('width',width);
-
-  var radius = Math.min(width,height)/2;
-
-  var arc = d3.svg.arc()
-    .outerRadius(radius - 10)
-    .innerRadius(0);
-
-  // set colors
-  var colors;
-  if (data.length <= 10){
-    colors = d3.scale.category10().range();
-  }
-  else if (data.length <= 20){
-    colors = d3.scale.category20().range();
-  }
-  else if (data.length <= 40){
-    colors = d3.scale.category20().range().concat(d3.scale.category20b().range());
-  }
-  else if (data.length <= 60){
-    colors = d3.scale.category20().range().concat(d3.scale.category20b().range()).concat(d3.scale.category20c().range());
-  }
-  else{
-    //if pie chart has more than 60 colors, return blank canvas
-    return canvas;
-  }
-
-  var pie = d3.layout.pie()
-    .sort(null)
-    .value(function(d) { return d; });
-
-  var g = canvas.selectAll(".arc")
-      .data(pie(data))
-      .enter().append("g")
-      .attr("class", "arc");
-
-  g.append("path")
-      .attr("d", arc)
-      .style("fill", function(d,i) { return colors[i]; });
-
-  g.append("text")
-      .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
-      .attr("dy", ".35em")
-      .style("text-anchor", "middle")
-      .text(function(d,i) { return config.labels[i]; });
-
-
-  g.attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-
-
-  return canvas;
-};
-
-
-// HISTO FUNCTION: creats histogram plot
-histo = function(data,config){
-  if (typeof config === 'undefined'){config = {}};
-  var xLab=config.xLab,selector=config.selector,canvasWidth=config.width,canvasHeight=config.height;
-
-  if(typeof canvasWidth === 'undefined'){
-    canvasWidth = 500;
-  }
-  if(typeof canvasHeight === 'undefined'){
-    canvasHeight = 500;
-  }
-  if(typeof selector === 'undefined'){
-    selector = 'body';
-  }
-  if(typeof xLab === 'undefined'){
-    xLab = '';
-  }
-
-  var hist = function(arr){
-    var newArr = arr.slice().sort(function(a,b){
-        return a-b;
-    });
-
-    var max = newArr[arr.length -1];
-    var min = newArr[0];
-    var bins = Math.round(Math.sqrt(arr.length));
-    var binSize = (max-min)/bins;
-
-    var obj= {};
-    var keys = [];
-    for (var i=0; i<bins; i++){
-        var key = min + (i*binSize);
-        keys.push(key);
-        obj[key] = 0;
-    }
-
-    for (var j=0; j<arr.length; j++){
-        var val = min;
-        var temp_key = 0;
-        while(true){
-            if (newArr[j] == newArr[newArr.length-1]){
-                obj[keys[keys.length-1]] += 1;
-                break;
-            }
-            else if (newArr[j]<val+binSize){
-                obj[keys[temp_key]]+= 1;
-                break;
-            }
-            else{
-                temp_key += 1;
-                val += binSize;
-            }
-        }
-    }
-
-      return [obj,min,max,binSize];
+  function build(datasets) {
+    buildCanvas();
+    buildXMap(datasets);
+    buildYMap(datasets);
+    buildAxes();
+    buildAxisLabels();
   };
 
-  var height = canvasHeight/1.3;
-  var width = canvasWidth/1.3;
-  if (canvasHeight - height < 75){height -= 45};
+  function getBoundaries(data) {
+    var min = Math.min.apply(null, data);
+    var max = Math.max.apply(null, data);
+    return [min, max];
+  };
 
-  var allData = hist(data);
-
-  var xMap = d3.scale.linear()
-                  .domain([allData[1],allData[2]])
-                  .range([0,width]);
-
-  var maxfreq = Math.max.apply( null,Object.keys(allData[0]).map(function ( key ) { return allData[0][key]; }) );
-
-  var yMap = d3.scale.linear()
-                  .domain([maxfreq,0])
-                  .range([0,height]);
-
-  var objects = start(xLab,'Frequency',xMap,yMap,canvasWidth,canvasHeight,width,height,selector);
-
-  var canvas = objects[0];
-  var everything = objects[1];
-
-  //MAKE AN ARRAY OF THE DATA TO BIND
-  var obj = allData[0];
-  var keys = Object.keys(obj);
-  var arr = [];
-  for (var i=0;i<keys.length;i++){
-      arr.push(obj[keys[i]]);
-  }
-
-  // obj,min,max,binSize
-  var binSize = xMap(allData[3] + allData[1]);
-  var padding = binSize * 0.075;
-  //padding used to create a buffer around each bin
-
-  everything.selectAll('rect')
-        .data(arr)
-        .enter()
-        .append('rect')
-        .attr('x', function(d,index){
-          return (index*binSize + padding/2);
-        })
-        .attr('y', function(d){
-          return yMap(d);
-        })
-        .attr('height', function(d){
-          return Math.max(yMap(maxfreq - d) - 0.5, 0);
-        })
-        .attr('width', binSize-padding)
-        .style('fill', 'steelBlue');
-
-  return canvas;
-};
-// END OF HIST FUNCTION
-
-// BEGINNING OF XY PLOT FUNCTION
-xyPlot = function(x,y,config){
-  if (typeof config === 'undefined'){config = {}};
-  var xLab=config.xLab,yLab=config.yLab,selector=config.selector,canvasWidth=config.width,canvasHeight=config.height;
-
-  if(typeof canvasWidth === 'undefined'){
-    canvasWidth = 500;
-  }
-  if(typeof canvasHeight === 'undefined'){
-    canvasHeight = 500;
-  }
-  if(typeof selector === 'undefined'){
-    selector = 'body';
-  }
-
-  var xSort = x.slice().sort(function(a,b){
-      return a-b;
-  });
-
-  var ySort = y.slice().sort(function(a,b){
-      return a-b;
-  });
-  var yMax = ySort[ySort.length-1];
-  var yMin = ySort[0];
-
-
-  var height = canvasHeight/1.3;
-  var width = canvasWidth/1.3;
-  if (canvasHeight - height < 75){height -= 45};
-
-  if (typeof x[0] !== 'number'){
-    if (typeof Date.parse(x[0]) === 'number'){
-      // if we're here, x[0] is a date
-      var xMap = d3.time.scale()
-                      .domain([new Date(x[0]),new Date(x[x.length-1])])
-                      .range([0,width]);
-      x.forEach(function(element,index){
-        x[index] = new Date(x[index]);
-      });
-    }
-  }
-  else{
-    // boundaries for numeric x
-    var xMax = xSort[xSort.length-1];
-    var xMin = xSort[0];
-
-    var xMap = d3.scale.linear()
-                    .domain([xMin,xMax])
-                    .range([0,width]);
-  }
-
-  var yMap = d3.scale.linear()
-                  .domain([yMax,yMin])
-                  .range([0,height]);
-
-  var objects = start(xLab,yLab,xMap,yMap,canvasWidth,canvasHeight,width,height,selector);
-
-  var canvas = objects[0];
-  var everything = objects[1];
-
-  for (var i=1;i<x.length;i++){
-    everything.append('line')
-              .attr('stroke-width',1)
-              .attr('stroke','black')
-              .attr('x1',xMap(x[i-1]))
-              .attr('x2',xMap(x[i]))
-              .attr('y1',yMap(y[i-1]))
-              .attr('y2',yMap(y[i]));
-  }
-
-  return canvas;
-
-};
-
-// START OF SCATTER FUNCTION
-scatter = function(x,y,config){
-  if (typeof config === 'undefined'){config = {}};
-  var xLab=config.xLab,yLab=config.yLab,selector=config.selector,canvasWidth=config.width,canvasHeight=config.height,z=config.size,zLab=config.sizeLab;
-
-  if(typeof canvasWidth === 'undefined'){
-    canvasWidth = 500;
-  }
-  if(typeof canvasHeight === 'undefined'){
-    canvasHeight = 500;
-  }
-  if(typeof selector === 'undefined'){
-    selector = 'body';
-  }
-
-  var xSort = x.slice().sort(function(a,b){
-        return a-b;
-  });
-
-  var ySort = y.slice().sort(function(a,b){
-        return a-b;
+  function getUniqueValues(data) {
+    var seenValues = {}, uniques = [];
+    data.forEach(function(val) {
+      if (seenValues[val] != 1) {
+        seenValues[val] = 1;
+        uniques.push(val);
+      };
     });
-
-  if (typeof z !== 'undefined'){
-    var zSort = z.slice().sort(function(a,b){
-          return a-b;
-    });
-  }
-
-  var yMax = ySort[ySort.length-1];
-  var yMin = ySort[0];
-
-  var height = canvasHeight/1.3;
-  var width = canvasWidth/1.3;
-  if (canvasHeight - height < 75){height -= 45};
-
-  if (typeof x[0] !== 'number'){
-    if (typeof Date.parse(x[0]) === 'number'){
-      // if we're here, x[0] is a date
-      var xMap = d3.time.scale()
-                      .domain([new Date(x[0]),new Date(x[x.length-1])])
-                      .range([0,width]);
-      x.forEach(function(element,index){
-        x[index] = new Date(x[index]);
-      });
-    }
-  }
-  else{
-    // boundaries for numeric x
-    var xMax = xSort[xSort.length-1];
-    var xMin = xSort[0];
-
-    var xMap = d3.scale.linear()
-                    .domain([xMin,xMax])
-                    .range([0,width]);
-  }
-
-  var yMap = d3.scale.linear()
-                  .domain([yMax,yMin])
-                  .range([0,height]);
-
-  if (typeof zLab !== 'undefined'){yLab = yLab+' ('+zLab+')'};
-  var objects = start(xLab,yLab,xMap,yMap,canvasWidth,canvasHeight,width,height,selector);
-
-  var canvas = objects[0];
-  var everything = objects[1];
-
-
-  x.forEach(function(elem,index){
-    everything.append('circle')
-              .attr('r',function(){
-                if (typeof z === 'undefined'){
-                  return height*width*(0.00002);
-                }
-                else{
-                  return (height*width*0.000025 + (z[index]-zSort[0])*(height*width*(0.0001))/(zSort[zSort.length-1] - zSort[0]));
-                }
-              })
-              .attr('cx',xMap(x[index]))
-              .attr('cy',yMap(y[index]))
-              .attr('opacity',function(){
-                if (typeof z === 'undefined'){
-                  return 1;
-                }
-                else{
-                  return 0.3;
-                }
-              })
-              .attr('fill',function(){
-                if (typeof z === 'undefined'){
-                  return 'none';
-                }
-                else{
-                  return 'steelBlue';
-                }
-              })
-              .attr('stroke', function(){
-                if (typeof z === 'undefined'){return'black'};
-                return 'none'
-              });
-  });
-
-  return canvas;
-
-}
-
+    return uniques;
+  };
+};
